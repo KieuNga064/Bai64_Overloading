626cea4167c813792b67f00ed0392e5e54049d64
KieuNga <KieuNga@kieunga-is>
2020-02-23 16:56:52 +0700

    phan 1

---
+++ Bai64HocOOP_OverloadingMethod/src/kieunga/com/test/TestSinhVien.java
@@ -0,0 +1,15 @@
+package kieunga.com.test;
+
+import kieunga.com.model.SinhVien;
+
+public class TestSinhVien {
+
+	public static void main(String[] args) {
+		SinhVien teo=new SinhVien(1, "Nguyễn Văn Tèo", 9);
+		teo.xetTotNghiep();
+		teo.xetTotNghiep(4);
+		teo.xetTotNghiep(9, 1);
+		
+	}
+
+}
